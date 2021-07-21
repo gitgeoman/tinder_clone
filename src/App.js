441ba7ff75f206791bd5db7b1df413bbc86321f3
gitgeoman <gitgeoman@gmail.com>
2021-07-22 00:01:34 +0200

    swiching with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 function App() {
   return (
     <div className="App">
       <h1>lets build Tinder clond</h1>
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/chat">I am chat</Route>
+        </Switch>
+        <Switch>
+          <Route exact strict path="/">
+            I am homepage
+          </Route>
+        </Switch>
+      </Router>
+
       {/*  tinder cards*/}
       {/* buttons blow tinder cards */}
     </div>
